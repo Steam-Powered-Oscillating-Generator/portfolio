0a0d1d029be6145694b5d1a15bf7cfc793bbfddd
Alejandro Mojarro <amojarrot@gmail.com>
2020-11-24 11:21:51 -0700

    hi2

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Lightsaber from '../images/lightsaber.png'
 
 const LandingPage = () => {
     return (
         <div>
             <div className='background2'>
                 <h1>Join the Nikola Tesla Team</h1>
-                <div>
+                <div className='formdiv'>
+                    <form>
+                        <input type="text" placeholder='First Name' />
+                        <input type="text" placeholder='Last Name' />
+                        <br />
+                        <input type="email" placeholder='Email Address' />
+                        <br />
+                        <input type="password" placeholder='Password' />
+                        <br />
+                        <button>Sign Up</button>
+                    </form>
+                    <img src={Lightsaber} alt="lightsaber" />
                 </div>
                 <h2>Why Nikola Tesla </h2>
                 <p>Tesla was a GENIUS! He spoke eighth languages - Serbian, English, Czech, German, France, Hungarian, Italian
