f5d5ca6627de696463eecbef5be0f27b39ec21e7
Bob <bkane90@gmail.com>
2020-11-24 15:24:17 -0500

    fedison1

--- src/components/FunPage.js
+++ src/components/FunPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactPlayer from "react-player"
+import TeslavsEdison from '../images/Tesla-vs-Edison.jpg'
 
 const FunPage = () => {
     return (
         <div>
-            About
+        <h2>Every Death Ray Has a Story</h2>
+        <img src={TeslavsEdison} alt="Tesla Vs Edison" />
+            <ReactPlayer
+            url="https://www.youtube.com/watch?v=3gOR91oentQ" />
         </div>
     );
 };
