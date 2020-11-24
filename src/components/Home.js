0cf49d02695731d67acb67983583f5cc77e04819
Alejandro Mojarro <amojarrot@gmail.com>
2020-11-24 15:00:41 -0700

    changes2

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,28 +20,42 @@ const Home = () => {
                 <img className='cover-image' src={Hero} alt="image" />
             </div>
             <div className='text-and-pic'>
-                <div>
-                    <h1>Why Nikola Tesla is better than Edison</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi veritatis tempore quam nobis labore temporibus fugiat laudantium mollitia dolorum suscipit inventore deserunt dolores error officia eos fuga, fugit culpa natus!</p>
+                <div className='textandimage'>
+                    <div>
+                        <h1>Why Nikola Tesla is better than Edison</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <br />Sequi veritatis tempore quam nobis labore temporibus fugiat
+                            <br />laudantium mollitia dolorum suscipit inventore deserunt dolores
+                            <br />error officia eos fuga, fugit culpa natus!
+                        </p>
+                    </div>
+                    <div>
+                        <img src={Master} alt="tesla pic" />
+                    </div>
                 </div>
-                <div>
-                    <img src={Master} alt="tesla pic" />
-                </div>
+
+
                 <img src={LightBulb} alt="light bulb" />
             </div>
             <div>
                 <h6>REVIEWS</h6>
                 <p>When asked what it felt to be the smartest man alive, Albert Einstein said, “ I don’t know, you have to ask Nikola Tesla!”</p>
             </div>
-            <img src={DeathRay} alt="death ray" />
-            <div>
-                <img src={Mi6} alt="mi6" />
-                <img src={Fbi} alt="fbi" />
-                <img src={Doj} alt="doj" />
-                <img src={Cern} alt="doj" />
-                <img src={Cia} alt="doj" />
-                <img src={Kgb} alt="doj" />
+            <div className='whatever'>
+                <div className='deathray'>
+                    <img src={DeathRay} alt="death ray" />
+                </div>
+                <div className="govagencies">
+                    <img src={Mi6} alt="mi6" />
+                    <img src={Fbi} alt="fbi" />
+                    <img src={Doj} alt="doj" />
+                    <img src={Cern} alt="doj" />
+                    <img src={Cia} alt="doj" />
+                    <img src={Kgb} alt="doj" />
+                </div>
             </div>
+
         </>
     );
 };
