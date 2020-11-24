30eb8394de1a7ea99b0eb357272ebc8ea38d944c
Alejandro Mojarro <amojarrot@gmail.com>
2020-11-24 13:35:12 -0700

    hiiil

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Navbar from './Navbar'
+// IMAGES
+import Hero from '../images/Hero.png'
+import Master from '../images/tesla-master-of-lightning.png'
 
 const Home = () => {
     return (
-        <div>
-           Home Page
-        </div>
+        <>
+            <div>
+                <img src={Hero} alt="image" />
+            </div>
+            <div>
+                <div>
+                    <h1></h1>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <img src={Master} alt="tesla pic" />
+                </div>
+            </div>
+        </>
     );
 };
 
